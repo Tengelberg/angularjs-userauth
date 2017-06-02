43db2b2f54c2ab8db5cda45f9247070edd8ff770
Nico <nico.meyer@enerxess.com>
2017-06-02 11:04:18 +0200

    Moved from bitbucket to github

---
+++ angular-userauth.js
@@ -0,0 +1,7 @@
+(function(window, angular) {'use strict';
+  angular.module('enerxess.UserAuth', [])
+  .factory('User', ['$cookies', function($cookies) {
+
+  }]);
+
+})(window, window.angular);
---
+++ index.js
@@ -0,0 +1,2 @@
+require('./angular-userauth.js');
+module.exports = 'enerxess.UserAuth';
